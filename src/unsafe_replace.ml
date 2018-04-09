8099e86132ae4b52611906737d22dc70dd8a7b18
J David Eisenberg <jdavid.eisenberg@gmail.com>
2018-04-09 09:20:05 -0700

    Rename function, fix bad backslashes.

--- src/unsafe_replace.ml
+++ src/unsafe_replace.ml
@@ -1,6 +1,6 @@
 type t = string
 
-external unsafeReplace : Js.Re.t ->  (t -> t array -> int -> t -> t [@bs.uncurry]) -> t -> t =
+external unsafeReplaceBy : Js.Re.t ->  (t -> t array -> int -> t -> t [@bs.uncurry]) -> t -> t =
   "unsafeReplaceConnector" [@@bs.module "./Connector"]
 
 (* Uppercase two words separated by a hyphen *)
@@ -9,7 +9,7 @@ let inStr1 = "Replace abc-def"
 let replaceFunction1 matchPart captures offset wholeString =
   Js.Array.joinWith "/" (Belt.Array.map captures (fun x -> Js.String.toUpperCase x))
 
-let outStr1 = unsafeReplace [%re "/(\\w+)-(\\w+)/"] replaceFunction1 inStr1
+let outStr1 = unsafeReplaceBy [%re "/(\\w+)-(\\w+)/"] replaceFunction1 inStr1
 let () = Js.log(outStr1)
 
 (* Replace expressions of form /(x0, y0) to (x1, y1)/ by the Pythagorean distance
@@ -17,9 +17,8 @@ let () = Js.log(outStr1)
 let inStr2 = "Distance (0, 3) to (4, 6)"
 let replaceFunction2 matchPart captures offset wholeString =
   let numbers = Belt.Array.map captures (fun x -> Js.Float.fromString(x)) in
-  let () = Js.log(numbers) in
   let d = sqrt ((numbers.(2) -. numbers.(0)) ** 2.0 +. (numbers.(3) -. numbers.(1)) ** 2.0) in
   Js.Float.toString(d)
   
-let outStr2 = unsafeReplace [%re "/\((\\d+), (\\d+)\) to \((\\d+), (\\d+)\)/"] replaceFunction2 inStr2
+let outStr2 = unsafeReplaceBy [%re "/\\((\\d+), (\\d+)\\) to \\((\\d+), (\\d+)\\)/"] replaceFunction2 inStr2
 let () = Js.log(outStr2)
